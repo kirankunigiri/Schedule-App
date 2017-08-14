c3327492f6199b3ea986f12ef3168aa5e02b398a
kirankunigiri <kirankunigiri@gmail.com>
2017-08-13 23:54:32 -0700

    Added Sketch design and created Xcode project

---
+++ Schedule/Schedule/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Schedule
+//
+//  Created by Kiran Kunigiri on 8/13/17.
+//  Copyright © 2017 Kiran. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
